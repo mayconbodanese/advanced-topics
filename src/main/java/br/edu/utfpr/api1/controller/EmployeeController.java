7dbc8d19788ea0a789a512f1afa0dcf263eebf6f
Maycon <mayconbodanese@hotmail.com>
2025-05-07 18:41:58 -0300

    Ultimo estado

--- src/main/java/br/edu/utfpr/api1/controller/EmployeeController.java
+++ src/main/java/br/edu/utfpr/api1/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -47,4 +48,13 @@ public class EmployeeController {
         return ResponseEntity.ok(employee);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        try {
+            employeeService.deleteEmployee(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
